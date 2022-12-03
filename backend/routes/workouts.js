0f36283c223cda34f4403d74f64f91f433ed9cc6
dinoelhadj <dino.elhadj@gmail.com>
2022-12-03 14:30:14 +0100

    init

---
+++ backend/routes/workouts.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const Workout = require('../models/workoutModel')
+
+const router = express.Router();
+
+// GET all workouts
+router.get("/", (req, res) => {
+  res.json({ mssg: "GET all workouts" });
+});
+
+// GET single workout
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "GET single workout" });
+});
+
+// POST a workout
+router.post("/", async (req, res) => {
+  const {title, reps, load} = req.body;
+  try {
+    const workout = await Workout.create({title, load, reps});
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+});
+
+// DELETE a workout
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "DELETE a workout" });
+});
+
+// UPDATE all workouts
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "UPDATE a workout" });
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+// express app
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use(express.json());
+app.use("/api/workouts", workoutRoutes);
+
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected To DB");
+
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Server Started | Listening on Port: ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
