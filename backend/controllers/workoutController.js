f3bdf025266ce8514e18c96331b1bb11b1748070
dinoelhadj <dino.elhadj@gmail.com>
2022-12-08 18:40:39 +0100

    update

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,3 @@
+const Workout = require('../models/workoutModel')
+
+//
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Workout = require('../models/workoutModel')
 
 const router = express.Router();
 
