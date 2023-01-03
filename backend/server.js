d70fba82391928a3fff45caec549c6634855124f
dinoelhadj <dino.elhadj@gmail.com>
2023-01-03 22:28:57 +0100

    ae

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const workoutRoutes = require("./routes/workouts");
 // express app
 const app = express();
 
-//middleware
+//middlewareAE
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
