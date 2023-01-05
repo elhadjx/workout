6d2cabd12de2649f0db397ee1f4e5bebd3353019
dinoelhadj <dino.elhadj@gmail.com>
2023-01-05 11:45:22 +0100

    modified:   backend/controllers/workoutController.js

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,3 +1,24 @@
 const Workout = require('../models/workoutModel')
 
-//
+// get all workouts
+
+// get a single workout
+
+// create new workout
+const createWorkout = async (req, res) => {
+    const {title, load, reps} = req.body
+
+    // add doc to db
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+}
+
+
+// delete a workout
+
+// update a workout
