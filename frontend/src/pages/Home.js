e9e6ee367e7d750558eee23292da8c0b7e491e7b
dinoelhadj <dino.elhadj@gmail.com>
2023-01-06 14:53:53 +0100

    qsd

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutFrom from '../components/WorkoutForm'
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -13,7 +13,6 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok){
-                setWorkouts(json)
             }
         }
 
